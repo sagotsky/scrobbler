bb14433a2083ae22c1b2fb39f955948bff2a479f
Jon Sagotsky <sagotsky@gmail.com>
2013-12-25 22:37:03 -0500

    Files are provided by stdin
    All I wanted was a shell command to submit mp3s to last.fm.  scrobbler.pl came closest.
    
    But I found the file/directory/random file thing disagreeable.  I want the shell command to just submit mp3s.  I can select those with echo, find, or find piped into shuf, and then pipe that whole mess into scrobbler.
    
    This version does that.  Pipe one file per line into scrobbler.pl and it'll send each of them to last.fm.  Make sure you use a -t delay - I got missing submissions when I tried scrobbling without that.
    
    Debating adding a check against my user's rss feed to avoid sending dupes.  That might be overengineernig and I'm not all that good with perl.

--- scrobbler.pl
+++ scrobbler.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
 
 ##
-#   (c) Denis Usanov 2011 :>
+#  scrobbler.pl
+#
+#  Scrobbles a single file specified with -f.
+# 
+#  Adapated from https://github.com/dck/scrobbler    (c) Denis Usanov 2011 :>
 ##
 
 use utf8;
@@ -11,106 +15,72 @@ use Audio::Scrobbler;
 use MP3::Info;
 use Cwd qw(abs_path cwd);
 use Getopt::Long;
+use XML::Feed;
 
 binmode STDOUT, ":utf8";
 
 my $login;
 my $password;
-my $dir;
-my $timeout     = 60;
 my $help        = 0;
-my $request_count = 25;
-my $count = 0;
+my $time        = 60;
 
 sub show_help
 {
-print <<HELP;
-Usage ./$0 -u <user> -p <password> -d <directory_or_file> [-t timeout] [-help]
-The script scrobbles random file from the directory (it may use a single file)
-with a specified interval.
--u <user>           - your login on las.fm
--p <password>       - your password on last.fm
--d <directory>      - path to directory with mp3 files to scrobble. 
-                      You can specify a signle file
--t <delay>          - an interval. Default value is 60 seconds
--h --help           - this text :>
-(c) Denis Usanov 2011
+  print <<HELP;
+  Usage ./$0 -u <user> -p <password> [-t time] [-help]
+  List of files provided by stdin will be sent to last.fm
+  -u <user>           - your login on last.fm
+  -p <password>       - your password on last.fm
+  -t <delay>          - Delay between requests.  Default: 60 seconds.
+  -h --help           - this text :>
+  (c) Denis Usanov 2011
 HELP
-    die("\n");
+  die("\n");
 }
-
-
-sub random_file
-{
-    my ($name) = @_;
-
-    if (-d $name)
-    {
-        chdir $name;
-        my @all_files = grep {!/^\.\.?$/} <*>;
-        my $rand_file = $all_files[int(rand(scalar @all_files))];
-        return random_file($rand_file);
-    } 
-    else
-    {
-        return abs_path($name);
-    }
-}
-
 
 GetOptions(
-    'user=s'        => \$login,
-    'password=s'    => \$password,
-    'dir=s'         => \$dir,
-    't=i'           => \$timeout,
-    'help'          => \$help,
+  'user=s'        => \$login,
+  'password=s'    => \$password,
+  'help'          => \$help,
+  't=i'           => \$time,
 ) or show_help;
 
-if ($help || !defined $login || !defined $password || !defined $dir)
+if ($help || !defined $login || !defined $password )
 {
-    show_help();
+  show_help();
 }
 
-while (1)
-{
-    my $connect = new Audio::Scrobbler(
-	    cfg => {
-		    progname	=> 'tst',
-		    progver		=> '1.4',
-		    username	=> $login,
-		    password	=> $password,
-		    verbose		=> 0,
-	    }
-    );
+my $connect = new Audio::Scrobbler(
+  cfg => {
+    progname	=> 'tst',
+    progver		=> '1.4',
+    username	=> $login,
+    password	=> $password,
+    verbose		=> 0,
+  }
+);
 
-    my $ua = $connect->get_ua();
+my $ua = $connect->get_ua();
+my $hs = $connect->handshake() or next and print "Error while connecting to last.fm: ".$connect->err;
+my $workdir = cwd();
+chdir $workdir;
 
-    my $hs = $connect->handshake() or next and print "Error while connecting to last.fm: ".$connect->err;
+foreach (<>) {
+  chomp;
+  my $tags = get_mp3tag("$_") or die;
+  my $info = get_mp3info("$_") or die;
+  my ($title, $artist, $album, $length) = ($tags->{TITLE}, $tags->{ARTIST}, $tags->{ALBUM}, $info->{MM}*60 + $info->{SS});
 
-    print "Connected to last.fm, starting scrobbling with interval ", $timeout, " seconds\n";
-    print "Press Ctrl+C to terminate\n";
-    
-    while (1)
+  my $submit = $connect->submit(
     {
-        my $workdir = cwd();
-        my $mp3file = random_file($dir);
-        chdir $workdir;
-        my $tags = get_mp3tag($mp3file) or next;
-        my $info = get_mp3info($mp3file) or next;
-        my ($title, $artist, $album, $length) = ($tags->{TITLE}, $tags->{ARTIST}, $tags->{ALBUM}, $info->{MM}*60 + $info->{SS});
-        
-        my $submit = $connect->submit(
-            {
-	            title	=> $title,
-	            artist	=> $artist,
-	            album	=> $album,
-	            length	=> $length,
-            }
-        ) or last and print "Error while submitting: ".$connect->err;
-        $count++;
-        print "$count: '$artist - $title' has been scrobbled\n";
-        sleep($timeout);
-        last if $count % $request_count == 0;
+      title	=> $title,
+      artist	=> $artist,
+      album	=> $album,
+      length	=> $length,
     }
-    print "Already sent $count requests, reconnecting...\n";
+  ) or last and print "Error while submitting: ".$connect->err;
+
+  print "'$artist - $title' has been scrobbled\n";
+
+  sleep($time);
 }
